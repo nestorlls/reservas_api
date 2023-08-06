52c4e7b2a51e79cd2f94538680edc4956ea72dc4
Paul Llanque <llanquepol@gmail.com>
2023-08-06 14:22:43 -0500

    [ADD] Reservation model added

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   User: require('./user.model'),
   Book: require('./book.model'),
+  Reservation: require('./reservation.model'),
 };
---
+++ src/models/reservation.model.js
@@ -0,0 +1,33 @@
+const { Schema } = require('mongoose');
+
+const ReservationSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      autopopulate: true,
+    },
+    book: {
+      type: Schema.Types.ObjectId,
+      ref: 'Book',
+      required: true,
+      autopopulate: true,
+    },
+    date_reserved: {
+      type: Date,
+      required: true,
+    },
+    date_due: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+Reservation.plugin(require('mongoose-autopopulate'));
+
+const Reservation = mongoose.model('Reservation', ReservationSchema);
+
+module.exports = Reservation;
