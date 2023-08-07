ca7069df83cadf105c1690a5465acf444f56a865
Paul Llanque <llanquepol@gmail.com>
2023-08-07 12:28:58 -0500

    [FIX] :userId in PUT method fixed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,7 +5,7 @@ module.exports = ({ UserController }) => {
 
   router.get('/', UserController.getAll);
   router.get('/:userId', UserController.getById);
-  router.put('/', UserController.update);
+  router.put('/:userId', UserController.update);
   router.delete('/:userId', UserController.delete);
 
   return router;
