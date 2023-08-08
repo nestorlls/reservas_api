8eaf593249dec2feea19b547b54a3429abaf0cf5
Paul Llanque <llanquepol@gmail.com>
2023-08-07 20:21:53 -0500

    [ADD] middleware authetication

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -1,13 +1,14 @@
 const { Router } = require('express');
+const { AuthMiddleware } = require('../middlewares');
 
 module.exports = ({ BookController }) => {
   const router = Router();
 
   router.get('/', BookController.getAll);
   router.get('/:bookId', BookController.getById);
-  router.post('/', BookController.create);
-  router.put('/:bookId', BookController.update);
-  router.delete('/:bookId', BookController.delete);
+  router.post('/', AuthMiddleware, BookController.create);
+  router.put('/:bookId', AuthMiddleware, BookController.update);
+  router.delete('/:bookId', AuthMiddleware, BookController.delete);
 
   return router;
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
+const { AuthMiddleware } = require('../middlewares');
 
 module.exports = ({ UserController }) => {
   const router = Router();
 
   router.get('/', UserController.getAll);
   router.get('/:userId', UserController.getById);
-  router.put('/:userId', UserController.update);
-  router.delete('/:userId', UserController.delete);
+  router.put('/:userId', AuthMiddleware, UserController.update);
+  router.delete('/:userId', AuthMiddleware, UserController.delete);
 
   return router;
 };
