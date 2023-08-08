e9677f1d64909a50abfcf60c2e89fb7bb8c3ef97
Paul Llanque <llanquepol@gmail.com>
2023-08-07 20:45:23 -0500

    [ADD] middleware cache

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,4 +2,5 @@ module.exports = {
   NotFoundMiddleware: require('./not-found.middleware'),
   ErrorMiddleware: require('./error.middleware'),
   AuthMiddleware: require('./auth.middleware'),
+  cacheMiddleware: require('./cache.middleware'),
 };
--- src/routes/reservation.routes.js
+++ src/routes/reservation.routes.js
@@ -1,13 +1,30 @@
 const { Router } = require('express');
-const { AuthMiddleware } = require('../middlewares');
+const { AuthMiddleware, cacheMiddleware } = require('../middlewares');
+const { CACHE_TIME } = require('../helpers');
 
 module.exports = ({ ReservationController }) => {
   const router = Router();
 
-  router.get('/', ReservationController.getReservations);
-  router.get('/:reservationId', ReservationController.getReservationById);
-  router.get('/:userId/all', ReservationController.getUserReservations);
-  router.get('/book/:bookId', ReservationController.getBookReservation);
+  router.get(
+    '/',
+    cacheMiddleware(CACHE_TIME.ONE_HOUR),
+    ReservationController.getReservations
+  );
+  router.get(
+    '/:reservationId',
+    cacheMiddleware(CACHE_TIME.ONE_HOUR),
+    ReservationController.getReservationById
+  );
+  router.get(
+    '/:userId/all',
+    cacheMiddleware(CACHE_TIME.ONE_HOUR),
+    ReservationController.getUserReservations
+  );
+  router.get(
+    '/book/:bookId',
+    cacheMiddleware(CACHE_TIME.ONE_HOUR),
+    ReservationController.getBookReservation
+  );
   router.post('/', AuthMiddleware, ReservationController.createReservation);
   router.put(
     '/:reservationId',
