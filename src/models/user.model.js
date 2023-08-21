057b38a66857f6701f644c11e7368ad8e0663afb
Paul Llanque <llanquepol@gmail.com>
2023-08-21 16:16:48 -0500

    [FIX] password field fixed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -6,7 +6,7 @@ const UserSchema = new Schema(
   {
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String },
   },
   {
     timestamps: true,
